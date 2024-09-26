c7e2e230a83a423866d010292b22bdd83fbb1e52
Leo <160294061+ptgz@users.noreply.github.com>
2024-09-26 16:31:04 -0300

    boboboleleletimtimtim

---
+++ main.py
@@ -0,0 +1,33 @@
+import test.test
+from lib.arquivo import escrever, pesquisar, exibir, excluir, alterar, gerarBoletim
+import lib.menu as menu
+
+menu.cls()
+while True:
+    menu.exibir()
+    opcao = menu.selecionar()
+
+    if opcao == "1":
+        ra = menu.obterRA()
+        aluno = pesquisar(ra)
+        if aluno:
+            exibir(ra, aluno)
+        else:
+            print("Aluno não encontrado")
+    elif opcao == "2":
+        dados = menu.obterDadosDoAluno()
+
+        escrever(dados)
+    elif opcao == "3":
+        ra = menu.excluirAluno()
+        excluir(ra)
+    elif opcao == "4":
+        ra = menu.alterarDadosAluno()
+        alterar(ra)
+    elif opcao == "5":
+        ra = menu.gerarBoletimAluno()
+        gerarBoletim(ra)
+    else:
+        break
+
+#ricardo barsellis megasoft
